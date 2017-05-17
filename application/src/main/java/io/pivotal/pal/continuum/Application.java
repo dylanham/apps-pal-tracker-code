493bcd8d93f0189b6955dffcf91745971cee5ba8
Sarah Niemeyer <sniemeyer13@gmail.com>
2017-05-17 10:16:34 -0600

    Build basic spring boot application

--- application/src/main/java/io/pivotal/pal/continuum/Application.java
+++ application/src/main/java/io/pivotal/pal/continuum/Application.java
@@ -1,8 +1,12 @@
 package io.pivotal.pal.continuum;
 
-class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Application.class, args);
     }
 }
