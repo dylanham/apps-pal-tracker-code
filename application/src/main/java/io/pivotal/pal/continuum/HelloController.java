66f5f7744cc92104fe1a5659220ca1e71a28da00
dylanham <dham@pivotal.io>
2017-05-25 08:42:20 -0600

    Add actuator and counter service metric for hello endpoint

--- application/src/main/java/io/pivotal/pal/continuum/HelloController.java
+++ application/src/main/java/io/pivotal/pal/continuum/HelloController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.continuum;
 
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,13 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     private final String message;
+    private final CounterService counterService;
 
-    public HelloController(String message) {
+    public HelloController(String message, CounterService counterService) {
         this.message = message;
+        this.counterService = counterService;
     }
 
     @GetMapping("/hello")
     public String sayHello() {
+        counterService.increment("continuum.counter.service.hello");
+
         return message;
     }
 }
