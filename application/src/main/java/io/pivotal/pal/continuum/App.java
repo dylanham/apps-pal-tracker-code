1006348afa379f89e1d03c6cd3e3080cf0c4b9c2
Damien Le Berrigaud <damien@itsdamo.com>
2017-05-26 10:13:35 -0600

    Write hello world spring boot application.

--- application/src/main/java/io/pivotal/pal/continuum/App.java
+++ application/src/main/java/io/pivotal/pal/continuum/App.java
@@ -1,8 +1,12 @@
 package io.pivotal.pal.continuum;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(App.class, args);
     }
 }
